f4552cf2552aa561797dc52b08a6a15accbaa840
arefev.ad <anton.arefiev@alp-erp.ru>
2024-10-17 22:49:37 +0300

    iter1: server up, middleware, handler method for update

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,25 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/arefev/mtrcstore/internal/http/middleware"
+)
+
+
+func updateMetric(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("update Metric"))
+}
+
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.Handle("/update/counter/someMetric/123", middleware.PostOnly(http.HandlerFunc(updateMetric)))
+
+	err := http.ListenAndServe(":8080", mux)
+	if (err != nil) {
+		panic(err)
+	}
+
+}
---
+++ internal/http/middleware/reqmethod.go
@@ -0,0 +1,14 @@
+package middleware
+
+import "net/http"
+
+func PostOnly(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if (r.Method != http.MethodPost) {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+		
+		next.ServeHTTP(w, r)
+	})
+}

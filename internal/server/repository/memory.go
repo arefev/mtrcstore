3cb399c4827de3bbcc97adde8d3d6d93ddf2365e
arefev.ad <anton.arefiev@alp-erp.ru>
2024-10-30 00:05:56 +0300

    iter5: fixed gauge string()

--- internal/server/repository/memory.go
+++ internal/server/repository/memory.go
@@ -9,7 +9,7 @@ type gauge float64
 type counter int64
 
 func (g gauge) String() string {
-	return strconv.FormatFloat(float64(g), 'f', 3, 64)
+	return strconv.FormatFloat(float64(g), 'f', -1, 64)
 }
 
 func (c counter) String() string {

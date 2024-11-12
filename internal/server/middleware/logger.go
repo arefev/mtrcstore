898bb40fd37c19dc04653b4d859225600ede33b0
arefev.ad <anton.arefiev@alp-erp.ru>
2024-11-12 14:38:54 +0300

    iter6: request log

--- internal/server/middleware/logger.go
+++ internal/server/middleware/logger.go
@@ -2,13 +2,25 @@ package middleware
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/arefev/mtrcstore/internal/server/logger"
+	"go.uber.org/zap"
 )
 
 func Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		logger.Log.Info(r.Method)
+		start := time.Now()
+
 		next.ServeHTTP(w, r)
+
+		duration := time.Since(start)
+
+		logger.Log.Info(
+			"Request handler",
+			zap.String("URI", r.RequestURI),
+			zap.String("method", r.Method),
+			zap.Duration("duration", duration),
+		)
 	})
 }

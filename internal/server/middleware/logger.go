bddf57696dac5172fa26cfc2d109a0c1fc7d9d27
arefev.ad <anton.arefiev@alp-erp.ru>
2024-11-12 15:23:26 +0300

    iter6: logging response status & size

--- internal/server/middleware/logger.go
+++ internal/server/middleware/logger.go
@@ -8,11 +8,46 @@ import (
 	"go.uber.org/zap"
 )
 
+type (
+    responseData struct {
+        status int
+        size int
+    }
+
+    loggingResponseWriter struct {
+        http.ResponseWriter
+        responseData *responseData
+    }
+)
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+    size, err := r.ResponseWriter.Write(b) 
+    r.responseData.size += size
+    return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	if statusCode == 0 {
+		statusCode = http.StatusOK
+	}
+
+    r.ResponseWriter.WriteHeader(statusCode) 
+    r.responseData.status = statusCode
+}
+
 func Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
-		next.ServeHTTP(w, r)
+		responseData := &responseData {
+            status: http.StatusOK,
+            size: 0,
+        }
+        lw := loggingResponseWriter {
+            ResponseWriter: w,
+            responseData: responseData,
+        }
+		next.ServeHTTP(&lw, r)
 
 		duration := time.Since(start)
 
@@ -20,7 +55,9 @@ func Logger(next http.Handler) http.Handler {
 			"Request handler",
 			zap.String("URI", r.RequestURI),
 			zap.String("method", r.Method),
+			zap.Int("status", responseData.status),
 			zap.Duration("duration", duration),
+			zap.Int("size", responseData.size),
 		)
 	})
 }

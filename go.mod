c3644006e3aeaecb023fbd44024efbc54ee9208f
arefev.ad <anton.arefiev@alp-erp.ru>
2024-11-13 00:54:06 +0300

    iter7: returned middleware logger

--- go.mod
+++ go.mod
@@ -15,7 +15,6 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/go-chi/chi v1.5.5
 	github.com/go-chi/chi/v5 v5.1.0
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/zap v1.27.0
--- internal/server/router.go
+++ internal/server/router.go
@@ -2,13 +2,13 @@ package server
 
 import (
 	"github.com/arefev/mtrcstore/internal/server/handler"
-	// "github.com/arefev/mtrcstore/internal/server/middleware"
+	"github.com/arefev/mtrcstore/internal/server/middleware"
 	"github.com/go-chi/chi/v5"
 )
 
 func InitRouter(h *handler.MetricHandlers) *chi.Mux {
 	r := chi.NewRouter()
-	// r.Use(middleware.Logger)
+	r.Use(middleware.Logger)
 
 	r.Get("/", h.Get)
 	r.Get("/value/{type}/{name}", h.Find)

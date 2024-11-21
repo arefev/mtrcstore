7c1d468ad7c4b7906e42c0b50cba1f8fb6aabee7
arefev.ad <anton.arefiev@alp-erp.ru>
2024-11-21 15:47:13 +0300

    iter10: added tests with mocks

--- cmd/server/main_test.go
+++ cmd/server/main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -8,13 +10,20 @@ import (
 	"github.com/arefev/mtrcstore/internal/server"
 	"github.com/arefev/mtrcstore/internal/server/handler"
 	"github.com/arefev/mtrcstore/internal/server/logger"
+	"github.com/arefev/mtrcstore/internal/server/mocks"
+	"github.com/arefev/mtrcstore/internal/server/model"
 	"github.com/arefev/mtrcstore/internal/server/repository"
 	"github.com/go-resty/resty/v2"
+	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func Test_main(t *testing.T) {
+func Test_UpdateFullUrl(t *testing.T) {
+	const interval = 300
+	const fileStoragePath = "./storage.json"
+	const restore = true
+			
 	type want struct {
 		urlPath      string
 		code         int
@@ -101,10 +110,6 @@ func Test_main(t *testing.T) {
 			cLog, err := logger.Build("debug")
 			require.NoError(t, err)
 
-			const interval = 300
-			const fileStoragePath = "./storage.json"
-			const restore = true
-
 			storage := repository.NewFile(interval, fileStoragePath, restore, cLog)
 			metricHandlers := handler.NewMetricHandlers(storage, cLog)
 
@@ -138,6 +143,164 @@ func Test_main(t *testing.T) {
 				}
 
 				assert.Equal(t, tValue, sValue)
+			}
+		})
+	}
+}
+
+func Test_Get(t *testing.T) {
+	type want struct {
+		value map[string]string
+	}
+	tests := []struct {
+		name string
+		want want
+	}{
+		{
+			name: "positive test 1 elem",
+			want: want{
+				value: map[string]string{
+					"PollCounter": "1",
+				},
+			},
+		},
+		{
+			name: "positive test 2 elems",
+			want: want{
+				value: map[string]string{
+					"PollCounter": "1",
+					"Alloc":       "2000",
+				},
+			},
+		},
+		{
+			name: "positive test empty",
+			want: want{
+				value: map[string]string{},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			storage := mocks.NewMockStorage(ctrl)
+			storage.EXPECT().Get().MaxTimes(1).Return(test.want.value)
+
+			cLog, err := logger.Build("debug")
+			require.NoError(t, err)
+
+			metricHandlers := handler.NewMetricHandlers(storage, cLog)
+
+			r := server.InitRouter(metricHandlers, cLog)
+			srv := httptest.NewServer(r)
+			defer srv.Close()
+
+			req := resty.New().R()
+			req.Method = http.MethodGet
+			req.URL = srv.URL
+
+			res, err := req.Send()
+			require.NoError(t, err)
+			require.Equal(t, http.StatusOK, res.StatusCode())
+
+			for k, v := range test.want.value {
+				require.Contains(
+					t,
+					string(res.Body()),
+					fmt.Sprintf("<li><strong>%s</strong>: %s</li>", k, v),
+				)
+			}
+		})
+	}
+}
+
+func Test_UpdateShortUrl(t *testing.T) {
+	const urlPath string = "/update"
+	var delta int64 = 1
+	var value float64 = 1.55
+
+	type want struct {
+		metric     model.Metric
+		err        error
+		statusCode int
+	}
+	tests := []struct {
+		name string
+		want want
+	}{
+		{
+			name: "positive test counter saved",
+			want: want{
+				metric: model.Metric{
+					ID:    "PollCounter",
+					MType: "counter",
+					Delta: &delta,
+				},
+				err:        nil,
+				statusCode: http.StatusOK,
+			},
+		},
+		{
+			name: "positive test gauge saved",
+			want: want{
+				metric: model.Metric{
+					ID:    "Alloc",
+					MType: "gauge",
+					Value: &value,
+				},
+				err:        nil,
+				statusCode: http.StatusOK,
+			},
+		},
+		{
+			name: "bad request, when returned error",
+			want: want{
+				metric: model.Metric{
+					ID:    "PollCounter",
+					MType: "counter",
+				},
+				err:        fmt.Errorf("saved failed"),
+				statusCode: http.StatusBadRequest,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			storage := mocks.NewMockStorage(ctrl)
+			storage.EXPECT().Save(test.want.metric).MaxTimes(1).Return(test.want.err)
+
+			cLog, err := logger.Build("debug")
+			require.NoError(t, err)
+
+			metricHandlers := handler.NewMetricHandlers(storage, cLog)
+
+			r := server.InitRouter(metricHandlers, cLog)
+			srv := httptest.NewServer(r)
+			defer srv.Close()
+
+			req := resty.New().R()
+			req.Method = http.MethodPost
+			req.SetHeader("Content-type", "application/json")
+			req.URL = srv.URL + urlPath
+
+			jsonValue, err := json.Marshal(test.want.metric)
+			require.NoError(t, err)
+
+			req.Body = jsonValue
+
+			res, err := req.Send()
+			require.NoError(t, err)
+			require.Equal(t, test.want.statusCode, res.StatusCode())
+
+			if test.want.err == nil {
+				require.Contains(t, string(res.Body()), string(jsonValue))
 			}
 		})
 	}
--- go.mod
+++ go.mod
@@ -9,8 +9,13 @@ require (
 )
 
 require (
+	github.com/golang/mock v1.6.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/mod v0.4.2 // indirect
 	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/tools v0.1.1 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
 
 require (

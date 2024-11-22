634b50f49b24174c7d062b2c6a6feb0e506a86be
arefev.ad <anton.arefiev@alp-erp.ru>
2024-11-22 15:51:02 +0300

    iter11: implementation func Save, Find

--- internal/server/repository/db.go
+++ internal/server/repository/db.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"fmt"
 	"time"
+	"errors"
 
 	"github.com/arefev/mtrcstore/internal/server/model"
 	"go.uber.org/zap"
@@ -42,7 +43,8 @@ func (rep *databaseRep) connect(dsn string) error {
 }
 
 func (rep *databaseRep) migrations() error {
-	ctx, cancel := context.WithTimeout(context.TODO(), 1*time.Second)
+	const timeCancel = time.Duration(1)
+	ctx, cancel := context.WithTimeout(context.TODO(), timeCancel*time.Second)
 	defer cancel()
 
 	if err := rep.createTableMetrics(ctx); err != nil {
@@ -66,23 +68,77 @@ func (rep *databaseRep) createTableMetrics(ctx context.Context) error {
 
 	_, err := rep.db.ExecContext(ctx, query)
 	if err != nil {
-		return fmt.Errorf("create table metrics failed: %w", err)
+		return fmt.Errorf("rep db createTableMetrics failed: %w", err)
 	}
 
 	return nil
 }
 
 func (rep *databaseRep) Save(m model.Metric) error {
+	const timeCancel = time.Duration(1)
+	ctx, cancel := context.WithTimeout(context.TODO(), timeCancel*time.Second)
+	defer cancel()
+
+	metric, err := rep.Find(m.ID, m.MType)
+	notFound := errors.Is(err, sql.ErrNoRows)
+
+	if notFound {
+		return rep.create(ctx, m)
+	} else if err == nil {
+		return rep.update(ctx, m, metric)
+	}
+
+	return fmt.Errorf("rep db Save failed: %w", err)
+}
+
+func (rep *databaseRep) create(ctx context.Context, m model.Metric) error {
+	query := "INSERT INTO metrics(type, name, value, delta) VALUES($1, $2, $3, $4)"
+	_, err := rep.db.ExecContext(ctx, query, m.MType, m.ID, m.Value, m.Delta)
+	if err != nil {
+		return fmt.Errorf("rep db create failed: %w", err)
+	}
+
+	return nil
+}
+
+func (rep *databaseRep) update(ctx context.Context, newMetric model.Metric, oldMetric model.Metric) error {
+	query := "UPDATE metrics SET value = $1, delta = $2 WHERE type = $3 AND name = $4"
+	_, err := rep.db.ExecContext(ctx, query, newMetric.Value, newMetric.Delta, oldMetric.MType, oldMetric.ID)
+	if err != nil {
+		return fmt.Errorf("rep db update failed: %w", err)
+	}
+
 	return nil
 }
 
 func (rep *databaseRep) Find(id string, mType string) (model.Metric, error) {
-	return model.Metric{}, nil
+	const timeCancel = time.Duration(1)
+	metric := model.Metric{}
+	ctx, cancel := context.WithTimeout(context.TODO(), timeCancel*time.Second)
+	defer cancel()
+
+	query := "SELECT type, name, value, delta FROM metrics WHERE type = $1 AND name = $2"
+	row := rep.db.QueryRowContext(ctx, query, mType, id)
+
+	err := row.Err()
+	if err != nil {
+		rep.log.Error("rep db Find failed", zap.Error(err))
+		return model.Metric{}, fmt.Errorf("rep db Find failed: %w", err)
+	}
+
+	err = row.Scan(&metric.MType, &metric.ID, &metric.Value, &metric.Delta)
+	if err != nil {
+		rep.log.Error("rep db Find failed", zap.Error(err))
+		return model.Metric{}, fmt.Errorf("rep db Find failed: %w", err)
+	}
+
+	return metric, nil
 }
 
 func (rep *databaseRep) Get() map[string]string {
+	const timeCancel = time.Duration(1)
 	list := make(map[string]string)
-	ctx, cancel := context.WithTimeout(context.TODO(), 1*time.Second)
+	ctx, cancel := context.WithTimeout(context.TODO(), timeCancel*time.Second)
 	defer cancel()
 
 	query := "SELECT type, name, value, delta FROM metrics ORDER BY type, name ASC"

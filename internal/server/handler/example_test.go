f01f361eb46870a63b39153374e0d79ad550151b
arefev.ad <anton.arefiev@alp-erp.ru>
2025-02-16 23:25:53 +0300

    iter18: example test

--- internal/server/handler/example_test.go
+++ internal/server/handler/example_test.go
@@ -1,38 +1,92 @@
-package handler
+package handler_test
 
 import (
+	"fmt"
 	"net/http"
+	"net/http/httptest"
+	"strconv"
 
+	"github.com/arefev/mtrcstore/internal/server"
+	"github.com/arefev/mtrcstore/internal/server/handler"
+	"github.com/arefev/mtrcstore/internal/server/logger"
+	"github.com/arefev/mtrcstore/internal/server/repository"
 	"github.com/go-resty/resty/v2"
 )
 
 func ExampleMetricHandlers_Update() {
+	cLog, err := logger.Build("debug")
+	if err != nil {
+		panic(err)
+	}
+	metricHandlers := handler.NewMetricHandlers(repository.NewMemory(), cLog)
+
+	r := server.InitRouter(metricHandlers, cLog, "")
+	srv := httptest.NewServer(r)
+	defer srv.Close()
+
 	req := resty.New().R()
 	req.Method = http.MethodPost
-	req.URL = "http://localhost:8080/update/gauge/Alloc/1.55"
+	req.URL = srv.URL + "/update/gauge/Alloc/1.55"
 
-	_, err := req.Send()
+	response, err := req.Send()
 	if err != nil {
 		panic(err)
 	}
 
+	fmt.Println("Response code: " + strconv.Itoa(response.StatusCode()))
+
 	// Output:
+	// Response code: 200
 }
 
 func ExampleMetricHandlers_Find() {
-	req := resty.New().R()
-	req.Method = http.MethodGet
-	req.URL = "http://localhost:8080/value/counter/PollCount"
+	cLog, err := logger.Build("debug")
+	if err != nil {
+		panic(err)
+	}
+	metricHandlers := handler.NewMetricHandlers(repository.NewMemory(), cLog)
 
-	_, err := req.Send()
+	r := server.InitRouter(metricHandlers, cLog, "")
+	srv := httptest.NewServer(r)
+	defer srv.Close()
+
+	req := resty.New().R()
+	req.Method = http.MethodPost
+	req.URL = srv.URL + "/update/gauge/Alloc/1.55"
+
+	_, err = req.Send()
 	if err != nil {
 		panic(err)
 	}
 
+	req = resty.New().R()
+	req.Method = http.MethodGet
+	req.URL = srv.URL + "/value/gauge/Alloc"
+
+	response, err := req.Send()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Response code: " + strconv.Itoa(response.StatusCode()))
+	fmt.Println("Response body: " + string(response.Body()))
+
 	// Output:
+	// Response code: 200
+	// Response body: 1.55
 }
 
 func ExampleMetricHandlers_UpdateJSON() {
+	cLog, err := logger.Build("debug")
+	if err != nil {
+		panic(err)
+	}
+	metricHandlers := handler.NewMetricHandlers(repository.NewMemory(), cLog)
+
+	r := server.InitRouter(metricHandlers, cLog, "")
+	srv := httptest.NewServer(r)
+	defer srv.Close()
+
 	data := `
 	{
 		"id": "PollCount",
@@ -42,64 +96,127 @@ func ExampleMetricHandlers_UpdateJSON() {
 	`
 	req := resty.New().R()
 	req.Method = http.MethodPost
-	req.URL = "http://localhost:8080/update/"
+	req.URL = srv.URL + "/update/"
 	req.Body = data
 
-	_, err := req.Send()
+	response, err := req.Send()
 	if err != nil {
 		panic(err)
 	}
 
+	fmt.Println("Response code: " + strconv.Itoa(response.StatusCode()))
+
 	// Output:
+	// Response code: 200
 }
 
 func ExampleMetricHandlers_FindJSON() {
-	data := `
-	{
-		"id": "PollCount",
-		"type": "counter"
+	cLog, err := logger.Build("debug")
+	if err != nil {
+		panic(err)
 	}
-	`
+	metricHandlers := handler.NewMetricHandlers(repository.NewMemory(), cLog)
+
+	r := server.InitRouter(metricHandlers, cLog, "")
+	srv := httptest.NewServer(r)
+	defer srv.Close()
+
 	req := resty.New().R()
 	req.Method = http.MethodPost
-	req.URL = "http://localhost:8080/value/"
-	req.Body = data
+	req.URL = srv.URL + "/update/gauge/Alloc/1.55"
 
-	_, err := req.Send()
+	_, err = req.Send()
 	if err != nil {
 		panic(err)
 	}
 
+	data := `
+	{
+		"id": "Alloc",
+		"type": "gauge"
+	}
+	`
+	req = resty.New().R()
+	req.Method = http.MethodPost
+	req.URL = srv.URL + "/value/"
+	req.Body = data
+
+	response, err := req.Send()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Response code: " + strconv.Itoa(response.StatusCode()))
+	fmt.Println("Response body: " + string(response.Body()))
+
 	// Output:
+	// Response code: 200
+	// Response body: {"value":1.55,"id":"Alloc","type":"gauge"}
 }
 
 func ExampleMetricHandlers_Get() {
+	cLog, err := logger.Build("debug")
+	if err != nil {
+		panic(err)
+	}
+	metricHandlers := handler.NewMetricHandlers(repository.NewMemory(), cLog)
+
+	r := server.InitRouter(metricHandlers, cLog, "")
+	srv := httptest.NewServer(r)
+	defer srv.Close()
+
 	req := resty.New().R()
 	req.Method = http.MethodGet
-	req.URL = "http://localhost:8080"
+	req.URL = srv.URL
 
-	_, err := req.Send()
+	response, err := req.Send()
 	if err != nil {
 		panic(err)
 	}
 
+	fmt.Println("Response code: " + strconv.Itoa(response.StatusCode()))
+
 	// Output:
+	// Response code: 200
 }
 
 func ExampleMetricHandlers_Ping() {
+	cLog, err := logger.Build("debug")
+	if err != nil {
+		panic(err)
+	}
+	metricHandlers := handler.NewMetricHandlers(repository.NewMemory(), cLog)
+
+	r := server.InitRouter(metricHandlers, cLog, "")
+	srv := httptest.NewServer(r)
+	defer srv.Close()
+
 	req := resty.New().R()
 	req.Method = http.MethodGet
-	req.URL = "http://localhost:8080/ping"
+	req.URL = srv.URL + "/ping"
 
-	_, err := req.Send()
+	response, err := req.Send()
 	if err != nil {
 		panic(err)
 	}
 
+	fmt.Println("Response code: " + strconv.Itoa(response.StatusCode()))
+
 	// Output:
+	// Response code: 200
 }
 
 func ExampleMetricHandlers_Updates() {
+	cLog, err := logger.Build("debug")
+	if err != nil {
+		panic(err)
+	}
+	metricHandlers := handler.NewMetricHandlers(repository.NewMemory(), cLog)
+
+	r := server.InitRouter(metricHandlers, cLog, "")
+	srv := httptest.NewServer(r)
+	defer srv.Close()
+
 	data := `
 	[
 		{
@@ -116,13 +233,16 @@ func ExampleMetricHandlers_Updates() {
 	`
 	req := resty.New().R()
 	req.Method = http.MethodPost
-	req.URL = "http://localhost:8080/updates/"
+	req.URL = srv.URL + "/updates/"
 	req.Body = data
 
-	_, err := req.Send()
+	response, err := req.Send()
 	if err != nil {
 		panic(err)
 	}
 
+	fmt.Println("Response code: " + strconv.Itoa(response.StatusCode()))
+
 	// Output:
+	// Response code: 200
 }

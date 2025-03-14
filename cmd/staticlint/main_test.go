496abbb746719e22ac56ef2e4d2ae2ab5de2741c
arefev.ad <anton.arefiev@alp-erp.ru>
2025-03-14 15:48:28 +0300

    iter23: test staticlint checkers

--- cmd/staticlint/main_test.go
+++ cmd/staticlint/main_test.go
@@ -3,11 +3,19 @@ package main
 import (
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	"golang.org/x/tools/go/analysis/analysistest"
 )
 
-func TestRun(t *testing.T) {
-	t.Run("staticlint run success", func(t *testing.T) {
+func TestOSExitCheckAnalyzer(t *testing.T) {
+	t.Run("staticlint OSExitCheckAnalyzer run success", func(t *testing.T) {
 		analysistest.Run(t, analysistest.TestData(), OSExitCheckAnalyzer, "./...")
+	})
+}
+
+func TestCheckers(t *testing.T) {
+	t.Run("staticlint checkers success", func(t *testing.T) {
+		checkers := checkers()
+		require.NotEmpty(t, checkers)
 	})
 }
